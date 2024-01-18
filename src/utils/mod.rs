49dbd9bc652a7fbf3980702378917899bd24d255
Maxence Cornaton <cirque.m@hotmail.fr>
2024-01-18 14:59:39 +0100

    Load `Credentials` with `env_manager`

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,13 +1,2 @@
 pub mod credentials;
 pub mod env_manager;
-
-pub fn retrieve_env_value_from_line<'a>(line: &'a str, key: &str) -> Option<&'a str> {
-    if line.starts_with(key) {
-        let option = line.split_once('=');
-        if option.is_some() {
-            return Some(option.unwrap().1.trim_end_matches('\r'));
-        }
-    }
-
-    None
-}
